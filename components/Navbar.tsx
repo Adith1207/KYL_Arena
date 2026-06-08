6fb1804553319cebc9b25079d4b2f6c8d1c724c3
Adith Narayan <adithnarayang@gmail.com>
2026-06-08 18:45:36 +0530

    changed the logo into official logo & changed some writing content

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -16,17 +16,28 @@ export default function Navbar() {
           {/* Logo & Subtitle */}
           <div className="flex items-center gap-3">
             {/* Multi-color sports logo */}
-            <svg className="h-8 w-8" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
-              {/* Blue element (runner arm/body) */}
-              <path d="M40 20 C 50 15, 60 25, 50 35 C 45 40, 35 30, 40 20 Z" fill="#3b82f6" />
-              {/* Green element (running leg) */}
-              <path d="M25 45 C 35 35, 50 45, 45 60 C 40 70, 30 65, 25 45 Z" fill="#22c55e" />
-              {/* Red element (torso/arm) */}
-              <path d="M55 40 C 65 30, 75 40, 70 55 C 65 65, 55 55, 55 40 Z" fill="#ef4444" />
-              {/* Yellow/Orange element (head/burst) */}
-              <circle cx="50" cy="22" r="7" fill="#eab308" />
-              {/* Dynamic athletic line */}
-              <path d="M15 75 Q 50 45, 85 75" stroke="#22c55e" strokeWidth="6" strokeLinecap="round" />
+            <svg className="h-10 w-10 shrink-0" viewBox="0 0 100 100" fill="none" xmlns="http://www.w3.org/2000/svg">
+              {/* Green Runner (Top Left) */}
+              <g fill="#22c55e">
+                <circle cx="48" cy="20" r="7" />
+                <path d="M 28 69 C 14 78, 12 79, 10 80 C 15 78, 25 65, 32 50 C 37 40, 48 30, 60 22 C 60 23, 56 28, 48 35 C 42 42, 34 50, 28 69 Z" />
+                <path d="M 33 48 C 30 35, 27 25, 24 20 C 24 23, 27 34, 33 48 Z" />
+                <path d="M 48 35 C 54 30, 58 29, 61 28 C 58 31, 52 35, 48 35 Z" />
+              </g>
+
+              {/* Red Cyclist (Top Right) */}
+              <g fill="#ef4444">
+                <circle cx="78" cy="32" r="7" />
+                <path d="M 46 48 C 58 40, 68 35, 75 42 C 68 49, 58 55, 50 58 C 55 52, 65 48, 72 45 C 65 42, 58 44, 46 48 Z" />
+                <path d="M 70 58 C 76 50, 85 52, 92 48 C 92 60, 82 72, 70 72 C 62 72, 65 60, 70 58 Z" stroke="#ef4444" strokeWidth="3" fill="none" />
+              </g>
+
+              {/* Blue Swimmer (Bottom) */}
+              <g fill="#3b82f6">
+                <circle cx="53" cy="68" r="7" />
+                <path d="M 6 81 C 12 83, 25 75, 35 68 C 45 61, 58 58, 68 62 C 60 62, 52 64, 42 70 C 32 76, 18 83, 6 81 Z" />
+                <path d="M 35 68 C 45 61, 58 58, 68 62 C 63 65, 55 70, 53 72 C 45 78, 30 83, 10 83 C 25 81, 32 74, 35 68 Z" />
+              </g>
             </svg>
             <div className="flex flex-col">
               <span className="text-lg font-black tracking-wider text-white leading-none">
